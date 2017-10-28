131032e975c0dc5d0bc4879d92755786ffea2ebe
Dominik Grzelak <grzelak.dominik@hotmail.com>
2017-10-28 18:15:24 +0200

    added event view

--- resources/views/events/view.blade.php
+++ resources/views/events/view.blade.php
@@ -1,7 +1,26 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Denis
- * Date: 28.10.2017
- * Time: 17:21
- */
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <h1 class="eventview-title">{{ $event->name }}</h1>
+                <h2 class="eventview-subtitle">{{ $event->location }}, {{ $event->date }}</h2>
+            </div>
+            <div class="col-md-4">
+                <img src="http://via.placeholder.com/200x200" class="img-thumbnail eventview-pic">
+            </div>
+        </div>
+        <div class="row" style="height: 40px"></div>
+        <div class="row">
+            <div class="col-md-6 col-md-offset-3">
+                <p class="eventview-desc">{{ $event->description }}</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-2 col-md-offset-8">
+                <button type="button" class="btn btn-primary">Sign up</button>
+            </div>
+        </div>
+    </div>
+@endsection
