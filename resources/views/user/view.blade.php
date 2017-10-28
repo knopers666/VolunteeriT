89515f57c3645efac58287f6149abef31e41649d
Cherasowy <cherras@outlook.com>
2017-10-28 18:49:33 +0200

    Userviev

--- resources/views/user/view.blade.php
+++ resources/views/user/view.blade.php
@@ -7,7 +7,7 @@
     <link href="{{asset('css/userviev.css')}}" rel ="stylesheet"/>
     <div class="container">
         <div class="row">
-            <div class="col-lg-6 col-sm-6">
+            <div class="col-md-offset-3 col-lg-6 col-sm-6">
                 <div class="card hovercard">
                     <div class="card-background">
                         <img class="card-bkimg" alt="" src="http://lorempixel.com/100/100/people/9/">
@@ -16,43 +16,34 @@
                     <div class="useravatar">
                         <img alt="" src="http://lorempixel.com/100/100/people/9/">
                     </div>
-                    <div class="card-info"> <span class="card-title">{{$user->name}}</span>
+                    <div class="card-info"> <span class="card-title">$user->name</span>
 
-                    </div>
-                </div>
-                <div class="btn-pref btn-group btn-group-justified btn-group-lg" role="group" aria-label="...">
-                    <div class="btn-group" role="group">
-                        <button type="button" id="stars" class="btn btn-primary" href="#tab1" data-toggle="tab"><span class="glyphicon glyphicon-star" aria-hidden="true"></span>
-                            <div class="hidden-xs">Your Events</div>
-                        </button>
-                    </div>
-                    <div class="btn-group" role="group">
-                        <button type="button" id="favorites" class="btn btn-default" href="#tab2" data-toggle="tab"><span class="glyphicon glyphicon-heart" aria-hidden="true"></span>
-                            <div class="hidden-xs">Search Event</div>
-                        </button>
-                    </div>
-                    <div class="btn-group" role="group">
-                        <button type="button" id="following" class="btn btn-default" href="#tab3" data-toggle="tab"><span class="glyphicon glyphicon-user" aria-hidden="true"></span>
-                            <div class="hidden-xs">Following</div>
-                        </button>
                     </div>
                 </div>
 
                 <div class="well">
                     <div class="tab-content">
                         <div class="tab-pane fade in active" id="tab1">
-                            <h3>This is tab 1</h3>
-                        </div>
-                        <div class="tab-pane fade in" id="tab2">
-                            <h3>This is tab 2</h3>
-                        </div>
-                        <div class="tab-pane fade in" id="tab3">
-                            <h3>This is tab 3</h3>
-                        </div>
+                            <h3>About me: </h3>
+                        <span class="about-me">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc viverra ligula sit amet orci facilisis laoreet. Donec non rutrum felis, quis tristique mauris. Nam imperdiet metus quis est gravida accumsan. Proin finibus nec diam sit amet auctor. Duis purus urna, molestie sit amet rhoncus tempor, tempor nec turpis. Phasellus hendrerit sodales interdum. Nullam id elit eu quam auctor vehicula. Donec nulla sapien, egestas eu dolor nec, mattis gravida ante.</span>
                     </div>
+                        <div class="tab-pane fade in active" id="tab2">
+                            <h4>Info: </h4>
+                            <div class="info">Phone number: $user->number</div>
+                            <div class="info">Age: $user->age</div>
+                            <div class="info">Email: $user->email</div>
+                        </div>
+                        <div class="tab-pane fade in active" id="tab3">
+                            <h4>Previous events: </h4>
+                            <div class="info">Event1</div>
+                            <div class="info">Event2</div>
+                            <div class="info">Event3</div>
+                            <div class="info">Event4</div>
+                        </div>
                 </div>
 
             </div>
         </div>
+    </div>
     </div>
 @endsection
